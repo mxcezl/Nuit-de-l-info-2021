cdcd76db993771b9cf4584c1af621179f72a5bac
AntoineHbnt <49656644+AntoineHbnt@users.noreply.github.com>
2021-12-03 03:44:25 +0100

    sauveteur v0.1

--- sauveteurdunkerquois/src/App.js
+++ sauveteurdunkerquois/src/App.js
@@ -1,10 +1,18 @@
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Sauveteur from "./pages/Sauveteur";
+import NotFound from "./pages/NotFound";
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      test
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/sauveteur" element={<Sauveteur/>} />
+        <Route component={NotFound}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ sauveteurdunkerquois/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./header.css"
+
+const Header = () => {
+    return (
+        <div id="header">
+            
+        </div>
+    );
+};
+
+export default Header;
