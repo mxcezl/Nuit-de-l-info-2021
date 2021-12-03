9680c59dbdb2004490162c8e701dfdfc66baf486
waddupp00 <ymadrzyk@gmail.com>
2021-12-03 03:24:44 +0100

    Enfin quelque chose de fonctionnel

--- main.c
+++ main.c
@@ -1,6 +1,57 @@
 #include "magic.h"
 #include <stdio.h>
 
-int main () {
-    return printf ( "Hello world && !\n" ) ;
+int main () { int input [ 3 ] ; int count = 0 ; _Bool flipSign = 0; int aAa ; goto A ;
+    //String "premier" ou "deuxième"
+    A : ;
+    char * pos = count ? "deuxième" : "premier" ;
+    goto aa ;
+    //Demande entrée nombre
+    aa : ;
+    printf ( "Entrez le %s nombre : " , pos ) ;
+    goto AA ;
+    //Demande entrée opérateur
+    aaa : ;
+    printf ( "Entrez l'opérateur (1=+,2=-,3=*,4=/) : " ) ;
+    goto AA ;
+    //Entrée utilisateur
+    AA : ;
+    scanf ( "%d" , & input [ count ++ ] ) ;
+    goto aaaa ;
+    //Aiguillage inputs
+    aaaa : ;
+    if ( count == 3 ) goto AAA ; else if ( count == 1 ) goto aaa ;
+    goto A ;
+    //Fin inputs, début traitement
+    AAA : ;
+    if ( input [ 1 ] == 3 ) goto AAAA ; else if ( input [ 1 ] == 1 ) goto AAAAA ; else if ( input [ 1 ] == 2 ) goto AAAAAA ; else if ( input [ 1 ] == 4 ) goto AAAAAAA ;
+    goto B ;
+    //Multiplication
+    AAAA : ;
+    if ( input [ 2 ] < 0 ) { flipSign = 1 ; input [ 2 ] = - input [ 2 ] ; goto AAAA ; } else { aAa = input [ 0 ] ; for ( int b = 1 ; b < input [ 2 ] ; b ++ ) input [ 0 ] += aAa ; }
+    goto AAAAAAAAAA ;
+    //Addition
+    AAAAA : ;
+    if ( input [ 2 ] ) if ( input [ 2 ] > 0 ) { input [ 0 ] ++ ; input [ 2 ] -- ; goto AAAAA ; } else { input [ 2 ] = - input [ 2 ] ; goto AAAAAA ; }
+    goto AAAAAAAAAA;
+    //Soustraction
+    AAAAAA : ;
+    if ( input [ 2 ] ) if ( input [ 2 ] > 0 ) { input [ 0 ] -- ; input [ 2 ] -- ; goto AAAAAA ; } else { input [ 2 ] = - input [ 2 ] ; goto AAAAA ; }
+    goto AAAAAAAAAA;
+    //Division
+    AAAAAAA : ;
+    if ( input [ 2 ] < 0 ) { flipSign = !flipSign; input [ 2 ] = - input [ 2 ] ; goto AAAAAAA ; } if ( input [ 0 ] < 0 ) { flipSign = 1; input [ 0 ] = - input [ 0 ] ; goto AAAAAAA ; } if ( input [ 2 ] == 0 ) goto BB ; for ( aAa = 0 ; input [ 0 ] >= input [ 2 ] ; aAa ++ ) input [ 0 ] -= input [ 2 ] ; if ( flipSign ) input [ 0 ] *= -1 ; printf ( "(Reste = %d) " , input [ 0 ]) ; input [ 0 ] = aAa ;
+    goto AAAAAAAAAA;
+    //Affichage résultat
+    AAAAAAAAAA : ;
+    if ( flipSign ) input [ 0 ] *= - 1 ; printf ( "Résultat = %d\n" , input [ 0 ] ) ;
+    return 0;
+    // Operateur inconnu
+    B : ;
+    printf ( "Opérateur inconnu\n" ) ;
+    return 1 ;
+    //Division par 0
+    BB : ;
+    printf ( "Division par zéro\n" ) ;
+    return 1 ;
 }
--- make.py
+++ make.py
@@ -1,38 +1,53 @@
 import os
+import random
 
 #First A is a latin A, second is a cyrillic A
 magic_ingredients = ['A','А']
 substitution_dict = {}
 
 #Generates a substitution dictionary and magic.h
-count = 0
+symbol_count = 1 # We start at 1 so we can safely name stuff in our code with latin As
+template = "#define {} {}"
 with open('symbols.txt','r') as symbols:
     with open('magic.h','w') as magic_h:
         symbol = symbols.readline()                 
         while symbol:
-            pattern = '{0:08b}'.format(count) #Converts count to binary
+            pattern = '{0:08b}'.format(symbol_count) #Converts count to binary
             substitution = ""
             for c in pattern:
                 substitution = substitution + magic_ingredients[int(c)] #0->Latin, 1->Cyrillic
-            model = "#define {} {}"
-            magic_h.write(model.format(substitution,symbol))
+            magic_h.write(template.format(substitution,symbol))
             substitution_dict[symbol.rstrip()] = substitution
-            count = count + 1
+            symbol_count = symbol_count + 1
             symbol = symbols.readline();
-to_write_magic = ""
+
+#Shuffling operation section for extra cancer
+section_begin = 4
+main_array = []
+with open('main.c','r') as main_c:
+    main_array = [x for x in main_c.read().split('\n') if not "//" in x] #Read original file and puts it in array without comment lines
+section_end = len(main_array) - 1
+main_sub_arrays = [main_array[i:i + 3] for i in range(section_begin, section_end, 3)] #Split into instruction sections (identifier, code, goto)
+random.Random(0xF15A4).shuffle(main_sub_arrays) #Shuffling
+main_new_array = [item for sublist in main_sub_arrays for item in sublist] #Flattening the list
+main_array[4:len(main_array) - 1]=main_new_array #Replacing old code with new shuffled code
 
 #Magic happens here
+to_write_magic = ""
+word_count = 0
+words_per_line = 12
 string_delimiter = 0 #If 1 : we're in a string
-with open('main.c','r') as main_c:
-    for main_line in main_c:
-        if "#include" in main_line: #So as to not break includes
-            to_write_magic = to_write_magic + main_line
-        else:
-            for word in main_line.split():
-                string_delimiter = (string_delimiter + word.count("\""))%2
-                if word in substitution_dict and string_delimiter < 1: #If we're not in a string, apply magic
+for main_line in main_array:
+    if "#include" in main_line: #So as to not break includes
+        to_write_magic = to_write_magic + main_line + "\n"
+    else:
+        for word in main_line.split():
+            string_delimiter = (string_delimiter + word.count("\""))%2
+            if string_delimiter < 1 : #If we're not in a string, apply magic
+                if word in substitution_dict:
                     word = substitution_dict[word]
-                to_write_magic = to_write_magic + word + " "
+                word_count = (word_count + 1) % words_per_line
+            to_write_magic = to_write_magic + word + ("\n" if word_count == words_per_line - 1 and string_delimiter < 1 else " ")
 with open('magic.c','w') as magic_c:
     magic_c.write(to_write_magic)
 
